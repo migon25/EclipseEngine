9c38316f288dd0ee718adaa4dc3901053fe56725
Miguel <miguel.garon@gmail.com>
2024-10-09 17:15:32 +0200

    vcpkg
    added vcpkg with MSBuild.

---
+++ EclipseEngine/EclipseEngine/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	printf("Hello, world!");
+	return 0;
+}
