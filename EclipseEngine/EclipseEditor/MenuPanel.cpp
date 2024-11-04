545dc91fcf27bafce439e5a084a795728a9dfba0
Miguel <miguel.garon@gmail.com>
2024-11-04 01:14:42 +0100

    UI change
    changed UI style. It's a bit more pleasing now ;)

--- EclipseEngine/EclipseEditor/MenuPanel.cpp
+++ EclipseEngine/EclipseEditor/MenuPanel.cpp
@@ -3,6 +3,7 @@
 #include "MenuPanel.h"
 #include <imgui.h>
 #include "EclipseEngine/Logger.h"
+#include <windows.h>
 
 MenuPanel::MenuPanel(const std::string& name, PanelHandler& panelHandler)
     : Panel(name), m_PanelHandler(panelHandler) {}
@@ -433,6 +434,20 @@ void MenuPanel::Render() {
 
 		if (ImGui::BeginMenu("Help")) {
 			ImGui::MenuItem("About", nullptr, nullptr);
+			if (ImGui::MenuItem("GitHub Repository")) {
+				// Open the GitHub repository in the default web browser
+				const char* url = "https://github.com/migon25/EclipseEngine";
+				#if defined(_WIN32)
+				ShellExecuteA(nullptr, "open", url, nullptr, nullptr, SW_SHOWNORMAL);
+				#elif defined(__APPLE__)
+				std::string openUrlCommand = "open " + std::string(url);
+				system(openUrlCommand.c_str());
+				#elif defined(__linux__)
+				std::string openUrlCommand = "xdg-open " + std::string(url);
+				system(openUrlCommand.c_str());
+				#endif
+			}
+
 			ImGui::EndMenu();
 		}
         ImGui::EndMainMenuBar();
